29278c9c5a134de528c9fad09620f44fe92cdae7
Shubham Tade <shubham.tade@gmail.com>
2025-04-05 09:30:59 +0530

    Testing Mobile Nav Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -138,7 +138,15 @@ const Navbar = () => {
           className="flex items-center text-gray-700"
           aria-label="Call support"
         >
-          <SupportAgentIcon className="h-5 w-5" />
+          {/* <SupportAgentIcon className="h-5 w-5" /> */}
+
+          <div className="relative flex items-center">
+            <SupportAgentIcon className="h-5 w-5" />
+            <span className="absolute bottom-0 left-2 flex h-2 w-2">
+              <span className="absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75 animate-ping"></span>
+              <span className="relative inline-flex h-2 w-2 rounded-full bg-green-500"></span>
+            </span>
+          </div>
         </a>
         <button
           onClick={toggleMenu}
